9761843b8f5be62ce64f510d0422ae58228aa9d0
sacha-smz <sacha.zacaropoulos@gmail.com>
2020-08-15 16:49:38 +0200

    refresh-token

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const app = express();
 
 const saltRounds = 10;
 
+// stockage dans des arrays pour la démo, vs. BDD ou Redis en conditions réelles
+const refreshTokens = [];
 const users = [];
 const records = [
   {
@@ -51,7 +53,13 @@ app.post("/users/login", async (req, res) => {
 
     for (const user of users) {
       if (user.name === name && (await bcrypt.compare(password, user.password))) {
-        return res.json({ data: { token: jwt.sign({ name }, process.env.JWT_SECRET) } });
+        // 40 secondes pour la démo, 10-15 min en conditions réelles
+        const token = jwt.sign({ name }, process.env.JWT_SECRET, { expiresIn: "40s" });
+
+        const refreshToken = jwt.sign({ name }, process.env.REFRESH_SECRET);
+        refreshTokens.push(refreshToken);
+
+        return res.json({ data: { token, refreshToken } });
       }
     }
 
@@ -60,6 +68,29 @@ app.post("/users/login", async (req, res) => {
     res.status(500).json({ errors: [err.toString()] });
     console.log(err);
   }
+});
+
+app.post("/refresh-token", (req, res) => {
+  const { refreshToken } = req.body;
+
+  if (!refreshToken) return res.status(401).json({ errors: ["Missing token"] });
+
+  if (!refreshTokens.includes(refreshToken)) return res.status(403).json({ errors: ["Invalid token"] });
+
+  jwt.verify(refreshToken, process.env.REFRESH_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ errors: ["Invalid token"] });
+
+    const token = jwt.sign({ name: user.name }, process.env.JWT_SECRET, { expiresIn: "40s" });
+    res.json({ data: { token } });
+  });
+});
+
+app.post("/logout", (req, res) => {
+  const refreshTokenIndex = refreshTokens.indexOf(req.body.refreshToken);
+  if (refreshTokenIndex > -1) {
+    refreshTokens.splice(refreshTokenIndex, 1);
+  }
+  res.json({ data: { success: true } });
 });
 
 app.get("/records", authMiddleware, (req, res) => {
